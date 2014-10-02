ae3e795c54a85cabc435383a06e28d982fb2cd16
unknown <b017444c@k106-2.student.staffs.ac.uk>
2014-10-02 12:33:12 +0100

    Added player class

---
+++ first_repo/first_repo/player.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace first_repo
+{
+	public class player
+	{
+		int playerPosX = 1;
+		int playerPosY = 1;
+		public int HitPoints;
+		
+		
+		public player ()
+		{
+		}
+	}
+}
+
